129d45f8096ded0ca7bcf66290b3574dbc1ff37a
0x800040005b <67833526+0x800040005b@users.noreply.github.com>
2021-04-05 21:38:43 +0300

    закончил верстать сайт, прикрутил js, адаптировал под мобилки

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     var body = document.body;
-    var sectionCount = document.querySelectorAll('section').length;
-
     var popup = document.querySelector('#popup');
     var popupModal = document.querySelector('#popup-modal');
+    var isScroll = false;
+    var firstSection;
 
     var orderConsultation = document.querySelector('#order_consultation');
 
@@ -13,21 +13,27 @@ document.addEventListener("DOMContentLoaded", function () {
     var popupClose = document.querySelector('#popup-close');
     var bulletContainer = document.querySelectorAll('.bullets');
     var sections = document.querySelectorAll('section');
+    firstSection = sections[0];
 
 
     renderBullet();
+    let bullets = firstSection.querySelectorAll('.bullet__link')
+    bullets[0].classList.add('active');
+
+
+
 
     /* Events */
 
     if (menuClose != null) {
-        menuClose.addEventListener('click', function (event) {
+        menuClose.addEventListener('click', function(event) {
             menu.classList.remove('active')
             burger.classList.remove('active');
             body.classList.remove('lock');
         });
     }
     if (burger != null) {
-        burger.addEventListener('click', function () {
+        burger.addEventListener('click', function() {
             burger.classList.add('active');
             menu.classList.add('active');
             body.classList.add('lock');
@@ -35,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
     if (popupClose != null) {
-        popupClose.addEventListener('click', function () {
+        popupClose.addEventListener('click', function() {
             popup.classList.remove('active')
             popupModal.classList.remove('active');
             body.classList.remove('lock');
@@ -44,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
     if (orderConsultation != null) {
-        orderConsultation.addEventListener('click', function (event) {
+        orderConsultation.addEventListener('click', function(event) {
             event.preventDefault();
             event.stopImmediatePropagation();
             popupModal.classList.add('active');
@@ -54,45 +60,72 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
     if (bulletContainer != null) {
-        for (var i = 0; i < bulletContainer.length; i++) {
-            bulletContainer.item(i).addEventListener('click', function (event) {
+        bulletContainer.forEach(function(currentContainer, indexContainer, bulletContainer) {
+            currentContainer.addEventListener('click', function(event) {
                 event.stopImmediatePropagation();
+                event.preventDefault();
                 let target = event.target;
-                let href = target.parentNode.getAttribute('href');
-                let links = document.querySelectorAll(`a.bullet__link[href="${href}"]`);
-                let bulletActive = document.querySelectorAll('span.bullet.active')
-                bulletActive.forEach(function (item,i, bulletActive) {
-                    item.classList.remove('active');
-                });
-                links.forEach(function (item, i, links) {
-                    item.querySelector('span.bullet').classList.add('active');
-
+                sections.forEach(function(currentSection, sectionIndex, sections) {
+                    if ("#" + currentSection.id == target.getAttribute('href')) {
+                        currentSection.scrollIntoView({ behavior: "smooth" });
+                        let bullets = currentSection.querySelectorAll('.bullets .bullet__link');
+                        bullets.forEach(function(currentBullet, bulletIndex, bullets) {
+                            if (currentBullet.getAttribute('href') == "#" + currentSection.id) {
+                                currentBullet.classList.add('active');
+                            }
+                        });
+                    }
                 });
             });
-        }
+        });
+
     }
-    document.addEventListener("scroll", function (event) {
-        console.log(window.scrollY);
+    window.addEventListener('scroll', function(event) {
+        sections.forEach(function(currentSection, indexSection, sections) {
+            if (getCoord(currentSection).top <= 0 && getCoord(currentSection).bottom >= 0) {
+                isScroll = true;
+                scrollBySection(currentSection, isScroll);
+            } else {
+                isScroll = false;
+                scrollBySection(currentSection, isScroll);
+
+            }
+
+        });
     });
 
     console.log('DOMContentLoaded');
 
     function createBullet(id) {
-        return `<a href="#${id}" class="bullet__link"><span class="bullet"></span></a>`
+        return `<a href="#${id}" class="bullet__link"></a>`
     }
 
     function renderBullet() {
         if (bulletContainer != null && bulletContainer.length > 0) {
             for (var i = 0; i < bulletContainer.length; i++) {
-                for (var j = 0; j < sectionCount; j++) {
+                for (var j = 0; j < sections.length; j++) {
                     let id = sections.item(j).getAttribute('id');
                     bulletContainer[i].insertAdjacentHTML('beforeend', createBullet(id));
                 }
-                let bullets = bulletContainer[i].querySelectorAll('.bullet');
-                bullets[0].classList.add('active');
             }
         }
     }
 
-});
+    function getCoord(element) {
+        if (element != null && typeof element.getBoundingClientRect().top != 'undefined' && typeof element.getBoundingClientRect().bottom != 'undefined')
+            return element.getBoundingClientRect();
+    }
 
+    function scrollBySection(section, isScroll) {
+        if (isScroll == true) {
+            let bullets = section.querySelectorAll('.bullet__link');
+            bullets.forEach(function(currentBullet, indexBullet, bullets) {
+                if (currentBullet.getAttribute('href') == "#" + section.id) {
+                    currentBullet.classList.add('active');
+                }
+            });
+        } else {
+            return;
+        }
+    }
+});
