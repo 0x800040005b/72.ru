2f5cb5d32c7e1a532c911762ba8065f3c8950cc8
0x800040005b <67833526+0x800040005b@users.noreply.github.com>
2021-04-06 12:12:41 +0300

    закончил сайт, сделал js, сделал адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var popup = document.querySelector('#popup');
     var popupModal = document.querySelector('#popup-modal');
     var isScroll = false;
-    var firstSection;
+    var firstSection = null;
 
     var orderConsultation = document.querySelector('#order_consultation');
 
     var burger = document.querySelector('#burger');
     var menu = document.querySelector('#menu');
+    var menuListLinks = document.querySelectorAll(".menu-list__link");
     var menuClose = document.querySelector('#menu-close');
     var popupClose = document.querySelector('#popup-close');
     var bulletContainer = document.querySelectorAll('.bullets');
     var sections = document.querySelectorAll('section');
     firstSection = sections[0];
-
 
     renderBullet();
     let bullets = firstSection.querySelectorAll('.bullet__link')
@@ -80,9 +80,29 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
     }
+    if (menuListLinks != null) {
+        menuListLinks.forEach(function(currentItemMenu, indexCurrentItemMenu, menuListLinks) {
+            currentItemMenu.addEventListener('click', function(event) {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                sections.forEach(function(currentSection, indexCurrent, sections) {
+                    if (currentItemMenu.getAttribute('href') == "#" + currentSection.id) {
+                        console.log(currentSection.id + ' = ' + currentItemMenu.getAttribute('href'));
+                        currentSection.scrollIntoView({ behavior: "smooth" });
+                        menu.classList.remove('active');
+                        body.classList.remove('lock');
+                        burger.classList.remove('active');
+
+                    }
+                });
+
+            });
+        });
+    }
+
     window.addEventListener('scroll', function(event) {
         sections.forEach(function(currentSection, indexSection, sections) {
-            if (getCoord(currentSection).top <= 0 && getCoord(currentSection).bottom >= 0) {
+            if (getCoord(currentSection).top <= 120 && getCoord(currentSection).bottom >= 0 || window.pageYOffset == 0) {
                 isScroll = true;
                 scrollBySection(currentSection, isScroll);
             } else {
@@ -117,14 +137,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function scrollBySection(section, isScroll) {
+        let bullets = null
         if (isScroll == true) {
-            let bullets = section.querySelectorAll('.bullet__link');
+            bullets = section.querySelectorAll('.bullet__link');
             bullets.forEach(function(currentBullet, indexBullet, bullets) {
+                currentBullet.classList.remove('active');
                 if (currentBullet.getAttribute('href') == "#" + section.id) {
                     currentBullet.classList.add('active');
                 }
             });
         } else {
+            bullets = section.querySelectorAll('.bullet__link');
+            bullets.forEach(function(currentBullet, currentIndex, bullets) {
+                if (currentBullet.classList.contains('active')) {
+                    currentBullet.classList.remove('active');
+                }
+            });
             return;
         }
     }
